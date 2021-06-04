0ebef2b19a7bb59b14224a5b4089a109380a0537
Gabriel Pizarro <raziel19_24@hotmail.com>
2021-06-04 17:25:46 -0600

    Agregar archivos de proyecto.

---
+++ BaseDatos/dbo/Tables/Vehiculo.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Vehiculo]
+(
+	VehiculoId INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Vehiculo PRIMARY KEY CLUSTERED(VehiculoId)
+	, MarcaVehiculo INT NOT NULL CONSTRAINT FK_Vehiculo_MarcaVehiculo FOREIGN KEY (MarcaVehiculId)
+	REFERENCES Dbo.MarcaVehiculo(MarcaVehiculo)
+	, Matricula VARCHAR(250) NOT NULL
+	, Color VARCHAR(250) NOT NULL
+	, Modelo VARCHAR NOT NULL
+	, FechaModelo DATE NOT NULL CONSTRAINT DE_Vehiculo_FechaModelo default('2020-01-01')
+	, Estado BIT NOT NULL 
+)
+WITH (DATA_COMPRESSION= PAGE)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX IDX_Vehiculo_Matricula
+ON dbo.Vehiculo(Matricula)
+WITH (DATA_COMPRESSION = PAGE)
+GO
+
+
+
+
+
+
+
---
+++ Entity/DBEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class DBEntity
+    {
+        public int CodeError { get; set; }
+        public String MsgError { get; set; }
+    }
+}
